fd623821fc96e7e7f1e505998b4f916590a1735d
Jangseongjin <sungjin5891@gmail.com>
2021-08-01 22:46:59 +0900

    백엔드 spring 으로 수정

--- backend/src/books/books.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm'
-import { BookEntity } from './entities/book.entity'
-
-@EntityRepository(BookEntity)
-export class BooksRepository extends Repository<BookEntity> {}
--- backend/src/books/dto/create-book.dto.ts
+++
@@ -1,5 +0,0 @@
-export class CreateBookDto {
-	title: string
-
-	writer: string
-}
